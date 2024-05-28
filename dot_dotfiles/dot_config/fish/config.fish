add6b63b7fb335c733f67c7ec6a9c746fbaffa53
Jack Allison <jacallis@cisco.com>
2024-05-28 11:38:18 -0400

    more!

---
+++ dot_dotfiles/dot_config/fish/config.fish
@@ -0,0 +1,29 @@
+set -Ux EDITOR nvim
+
+fish_vi_key_bindings
+
+# Aliases
+alias lg lazygit
+alias tf terraform
+alias p "poetry run"
+alias dc "docker compose"
+alias k "kubectl"
+alias cat "bat"
+alias ls "lsd"
+alias la "lsd -la"
+
+# iTerm integration
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
+
+# VS Code integration
+string match -q "$TERM_PROGRAM" vscode
+and . (code --locate-shell-integration-path fish)
+
+# Bat Theme
+set -Ux BAT_THEME "TwoDark"
+
+# Set up Zoxide
+zoxide init fish | source
+
+# Setup Starship
+starship init fish | source
