222ea4f2d5bad8fa0256ce196f87f2ea17097483
Jack Allison <jacallis@cisco.com>
2024-06-21 13:43:14 -0400

    add mini surround

--- dot_config/nvim/lua/plugins/tools.lua
+++ dot_config/nvim/lua/plugins/tools.lua
@@ -1,4 +1,25 @@
 return {
+  {
+    'echasnovski/mini.surround',
+    version = false,
+    config = function()
+      require('mini.surround').setup {
+        mappings = {
+          add = 'ysa', -- Add surrounding in Normal and Visual modes
+          delete = 'ysd', -- Delete surrounding
+          find = 'ysf', -- Find surrounding (to the right)
+          find_left = 'ysF', -- Find surrounding (to the left)
+          highlight = 'ysh', -- Highlight surrounding
+          replace = 'ysr', -- Replace surrounding
+          update_n_lines = 'ysn', -- Update `n_lines`
+
+          suffix_last = 'yl', -- Suffix to search with "prev" method
+          suffix_next = 'yn', -- Suffix to search with "next" method
+        },
+      }
+    end,
+  },
+
   -- Folds that make sense
   {
     'kevinhwang91/nvim-ufo',
