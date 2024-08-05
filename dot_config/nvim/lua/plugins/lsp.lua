942a9dd3ebb675c7de495ff686e466c6d620783b
Jack Allison <jacallis@cisco.com>
2024-08-05 17:13:32 -0400

    disable copilot for markdown

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -247,6 +247,7 @@ return {
 		"github/copilot.vim",
 		config = function()
 			vim.g.copilot_no_tab_map = true
+			vim.g.copilot_filetypes = { markdown = false }
 			vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 		end,
 	},
