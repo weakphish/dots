4f84dda1d0d1d3b399f51390d22570dcc287dbb6
Jack Allison <jacallis@cisco.com>
2024-08-07 13:58:50 -0400

    set tabstop

--- dot_config/nvim/lua/settings.lua
+++ dot_config/nvim/lua/settings.lua
@@ -7,6 +7,12 @@
   -- [[ Setting general options ]]
   -- Set highlight on search
   vim.o.hlsearch = false
+  
+  -- Use 4 spaces for indentation
+  vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+  vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+  vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+  vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
   -- Make line numbers default
   vim.wo.number = true
