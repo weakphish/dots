14b17fae041cd3307c3eec960156608d28faa267
Jack Allison <jacallis@cisco.com>
2024-10-28 11:05:02 -0400

    refacotring

--- config/dot-config/nvim/lua/plugins/tools.lua
+++ config/dot-config/nvim/lua/plugins/tools.lua
@@ -1,63 +1,63 @@
 return {
-	{
-		"echasnovski/mini.surround",
-		version = false,
-		config = function()
-			require("mini.surround").setup({
-				mappings = {
-					add = "gsa", -- Add surrounding in Normal and Visual modes
-					delete = "gsd", -- Delete surrounding
-					find = "gsf", -- Find surrounding (to the right)
-					find_left = "gsF", -- Find surrounding (to the left)
-					highlight = "gsh", -- Highlight surrounding
-					replace = "gsr", -- Replace surrounding
-					update_n_lines = "gsn", -- Update `n_lines`
-				},
-			})
-		end,
-	},
+  {
+    'echasnovski/mini.surround',
+    version = false,
+    config = function()
+      require('mini.surround').setup {
+        mappings = {
+          add = 'gsa', -- Add surrounding in Normal and Visual modes
+          delete = 'gsd', -- Delete surrounding
+          find = 'gsf', -- Find surrounding (to the right)
+          find_left = 'gsF', -- Find surrounding (to the left)
+          highlight = 'gsh', -- Highlight surrounding
+          replace = 'gsr', -- Replace surrounding
+          update_n_lines = 'gsn', -- Update `n_lines`
+        },
+      }
+    end,
+  },
 
-	-- Folds that make sense
-	{
-		"kevinhwang91/nvim-ufo",
-		dependencies = "kevinhwang91/promise-async",
-		config = function()
-			vim.o.foldcolumn = "0" -- '0' is not bad
-			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-			vim.o.foldlevelstart = 99
-			vim.o.foldenable = true
+  -- Folds that make sense
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = 'kevinhwang91/promise-async',
+    config = function()
+      vim.o.foldcolumn = '0' -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
 
-			-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-			vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-			vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+      -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
-			--  treesitter as a main provider instead
-			-- Only depend on `nvim-treesitter/queries/filetype/folds.scm`,
-			-- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
-			require("ufo").setup({
-				provider_selector = function(bufnr, filetype, buftype)
-					return { "treesitter", "indent" }
-				end,
-			})
-		end,
-	},
+      --  treesitter as a main provider instead
+      -- Only depend on `nvim-treesitter/queries/filetype/folds.scm`,
+      -- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
+      require('ufo').setup {
+        provider_selector = function(bufnr, filetype, buftype)
+          return { 'treesitter', 'indent' }
+        end,
+      }
+    end,
+  },
 
-	-- Society has advanced past the need for a mouse
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		---@type Flash.Config
-		opts = {
-			modes = {
-				-- options used when flish is activated through
-				-- a regular search with `/` or `?`
-				search = {
-					-- when `true`, flash will be activated during regular search by default.
-					-- You can always toggle when searching with `require("flash").toggle()`
-					enabled = true,
-				},
-			},
-		},
+  -- Society has advanced past the need for a mouse
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {
+      modes = {
+        -- options used when flish is activated through
+        -- a regular search with `/` or `?`
+        search = {
+          -- when `true`, flash will be activated during regular search by default.
+          -- You can always toggle when searching with `require("flash").toggle()`
+          enabled = true,
+        },
+      },
+    },
 	        -- stylua: ignore
 	        keys = {
 			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
@@ -66,158 +66,173 @@ return {
 			{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
 			{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
 	        },
-	},
+  },
 
-	-- Fuzzy Finder (files, lsp, etc)
-	{
-		"nvim-telescope/telescope.nvim",
-		version = "*",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"debugloop/telescope-undo.nvim",
-		},
-	},
+  -- Fuzzy Finder (files, lsp, etc)
+  {
+    'nvim-telescope/telescope.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'debugloop/telescope-undo.nvim',
+    },
+  },
 
-	-- Fuzzy Finder Algorithm which requires local dependencies to be built.
-	-- Only load if `make` is available. Make sure you have the system
-	-- requirements installed.
-	{
-		"nvim-telescope/telescope-fzf-native.nvim",
-		-- NOTE: If you are having trouble with this installation,
-		--       refer to the README for telescope-fzf-native for more instructions.
-		build = "make",
-		cond = function()
-			return vim.fn.executable("make") == 1
-		end,
-	},
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+  -- Only load if `make` is available. Make sure you have the system
+  -- requirements installed.
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    -- NOTE: If you are having trouble with this installation,
+    --       refer to the README for telescope-fzf-native for more instructions.
+    build = 'make',
+    cond = function()
+      return vim.fn.executable 'make' == 1
+    end,
+  },
 
-	-- Show files adjacent to current buffer
-	{
-		"MaximilianLloyd/adjacent.nvim",
-	},
+  -- Show files adjacent to current buffer
+  {
+    'MaximilianLloyd/adjacent.nvim',
+  },
 
-	-- Highlight, edit, and navigate code
-	{
-		"nvim-treesitter/nvim-treesitter",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-		config = function()
-			vim.treesitter.language.register("markdown", "octo")
+  -- Highlight, edit, and navigate code
+  {
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    config = function()
+      vim.treesitter.language.register('markdown', 'octo')
 
-			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
-			-- See `:help telescope` and `:help telescope.setup()`
-			require("telescope").setup({
-				defaults = {
-					mappings = {
-						i = {
-							["<C-u>"] = false,
-							["<C-d>"] = false,
-						},
-					},
-				},
-			})
+      pcall(require('nvim-treesitter.install').update { with_sync = true })
+      -- See `:help telescope` and `:help telescope.setup()`
+      require('telescope').setup {
+        defaults = {
+          mappings = {
+            i = {
+              ['<C-u>'] = false,
+              ['<C-d>'] = false,
+            },
+          },
+        },
+      }
 
-			-- Enable telescope fzf native, if installed
-			pcall(require("telescope").load_extension, "fzf")
-			-- Enable telescope undo
-			pcall(require("telescope").load_extension, "undo")
-			-- Enable telescope with adjacanet
-			require("telescope").load_extension("adjacent")
+      -- Enable telescope fzf native, if installed
+      pcall(require('telescope').load_extension, 'fzf')
+      -- Enable telescope undo
+      pcall(require('telescope').load_extension, 'undo')
+      -- Enable telescope with adjacanet
+      require('telescope').load_extension 'adjacent'
 
-			-- [[ Configure Treesitter ]]
-			-- See `:help nvim-treesitter`
-			require("nvim-treesitter.configs").setup({
-				-- Add languages to be installed here that you want installed for treesitter
-				ensure_installed = {},
+      -- [[ Configure Treesitter ]]
+      -- See `:help nvim-treesitter`
+      require('nvim-treesitter.configs').setup {
+        -- Add languages to be installed here that you want installed for treesitter
+        ensure_installed = {},
 
-				-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-				auto_install = true,
+        -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+        auto_install = true,
 
-				highlight = { enable = true },
-				indent = { enable = true, disable = { "python" } },
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<c-space>",
-						node_incremental = "<c-space>",
-						scope_incremental = "<c-s>",
-						node_decremental = "<M-space>",
-					},
-				},
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-						keymaps = {
-							-- You can use the capture groups defined in textobjects.scm
-							["aa"] = "@parameter.outer",
-							["ia"] = "@parameter.inner",
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-						},
-					},
-					move = {
-						enable = true,
-						set_jumps = true, -- whether to set jumps in the jumplist
-						goto_next_start = {
-							["]m"] = "@function.outer",
-							["]]"] = "@class.outer",
-						},
-						goto_next_end = {
-							["]M"] = "@function.outer",
-							["]["] = "@class.outer",
-						},
-						goto_previous_start = {
-							["[m"] = "@function.outer",
-							["[["] = "@class.outer",
-						},
-						goto_previous_end = {
-							["[M"] = "@function.outer",
-							["[]"] = "@class.outer",
-						},
-					},
-					swap = {
-						enable = true,
-						swap_next = {
-							["<leader>a"] = "@parameter.inner",
-						},
-						swap_previous = {
-							["<leader>A"] = "@parameter.inner",
-						},
-					},
-				},
-			})
-		end,
-	},
+        highlight = { enable = true },
+        indent = { enable = true, disable = { 'python' } },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<c-space>',
+            node_incremental = '<c-space>',
+            scope_incremental = '<c-s>',
+            node_decremental = '<M-space>',
+          },
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.inner',
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              [']m'] = '@function.outer',
+              [']]'] = '@class.outer',
+            },
+            goto_next_end = {
+              [']M'] = '@function.outer',
+              [']['] = '@class.outer',
+            },
+            goto_previous_start = {
+              ['[m'] = '@function.outer',
+              ['[['] = '@class.outer',
+            },
+            goto_previous_end = {
+              ['[M'] = '@function.outer',
+              ['[]'] = '@class.outer',
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ['<leader>a'] = '@parameter.inner',
+            },
+            swap_previous = {
+              ['<leader>A'] = '@parameter.inner',
+            },
+          },
+        },
+      }
+    end,
+  },
 
-	-- Highlight / search TODO
-	{
-		"folke/todo-comments.nvim",
-		config = function()
-			require("todo-comments").setup({})
-		end,
-	},
+  -- Highlight / search TODO
+  {
+    'folke/todo-comments.nvim',
+    config = function()
+      require('todo-comments').setup {}
+    end,
+  },
 
-	-- VS Code-like term
-	{ "akinsho/toggleterm.nvim", version = "*", config = true },
+  -- VS Code-like term
+  { 'akinsho/toggleterm.nvim', version = '*', config = true },
 
-	-- LazyGit in Neovim
-	-- nvim v0.8.0
-	{
-		"kdheepak/lazygit.nvim",
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
-		},
-		-- optional for floating window border decoration
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-	},
+  -- LazyGit in Neovim
+  -- nvim v0.8.0
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+  },
+
+  -- Refactoring tools
+  {
+    'ThePrimeagen/refactoring.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    lazy = false,
+    config = function()
+      require('refactoring').setup()
+      -- load refactoring Telescope extension
+      require('telescope').load_extension 'refactoring'
+    end,
+  },
 }
--- config/dot-config/nvim/lua/plugins/which-key.lua
+++ config/dot-config/nvim/lua/plugins/which-key.lua
@@ -1,63 +1,74 @@
 return {
-	-- Show key options when using shortcuts
-	"folke/which-key.nvim",
-	opts = {},
-	config = function()
-		local wk = require("which-key")
+  -- Show key options when using shortcuts
+  'folke/which-key.nvim',
+  opts = {},
+  config = function()
+    local wk = require 'which-key'
 
-		-- Normal mode
-		wk.add({
-			{ "<leader>b", group = "buffer" },
-			{ "<leader>bb", require("telescope.builtin").buffers, desc = "Find Buffer" },
-			{ "<leader>bd", "<cmd>bd<CR>", desc = "Delete Buffer" },
-			{ "<leader>bn", "<cmd>bn<CR>", desc = "Next Buffer" },
-			{ "<leader>bp", "<cmd>bp<CR>", desc = "Prev Buffer" },
-			{ "<leader>bo", "<cmd>BufferLinePick<CR>", desc = "Pick Buffer From Line" },
+    -- Normal mode
+    wk.add {
+      { '<leader>b', group = 'buffer' },
+      { '<leader>bb', require('telescope.builtin').buffers, desc = 'Find Buffer' },
+      { '<leader>bd', '<cmd>bd<CR>', desc = 'Delete Buffer' },
+      { '<leader>bn', '<cmd>bn<CR>', desc = 'Next Buffer' },
+      { '<leader>bp', '<cmd>bp<CR>', desc = 'Prev Buffer' },
+      { '<leader>bo', '<cmd>BufferLinePick<CR>', desc = 'Pick Buffer From Line' },
 
-			{ "<leader>c", group = "code" },
-			{ "<leader>ca", vim.lsp.buf.code_action, desc = "Code Action" },
-			{ "<leader>cA", require("tiny-code-action").code_action, desc = "Visualize Code Actions" },
-			{ "<leader>cd", vim.lsp.buf.definition, desc = "Go To Definition" },
-			{ "<leader>cD", vim.lsp.buf.declaration, desc = "Go to Declaration" },
-			{ "<leader>ci", vim.lsp.buf.implementation, desc = "Go to Implementation" },
-			{ "<leader>cf", require("conform").format, desc = "Format Buffer" },
-			{ "<leader>cp", "<cmd>Copilot enable<CR>", desc = "Enable Copilot" },
-			{ "<leader>cP", "<cmd>Copilot disable<CR>", desc = "Disable Copilot" },
-			{ "<leader>cr", vim.lsp.buf.rename, desc = "Code Rename" },
-			{ "<leader>ct", vim.lsp.buf.type_definition, desc = "Go To Type Definition" },
+      { '<leader>c', group = 'code' },
+      { '<leader>ca', vim.lsp.buf.code_action, desc = 'Code Action' },
+      { '<leader>cd', vim.lsp.buf.definition, desc = 'Go To Definition' },
+      { '<leader>cD', vim.lsp.buf.declaration, desc = 'Go to Declaration' },
+      { '<leader>ci', vim.lsp.buf.implementation, desc = 'Go to Implementation' },
+      { '<leader>cf', require('conform').format, desc = 'Format Buffer' },
+      { '<leader>cp', '<cmd>Copilot enable<CR>', desc = 'Enable Copilot' },
+      { '<leader>cP', '<cmd>Copilot disable<CR>', desc = 'Disable Copilot' },
+      { '<leader>cr', vim.lsp.buf.rename, desc = 'Code Rename' },
+      { '<leader>ct', vim.lsp.buf.type_definition, desc = 'Go To Type Definition' },
 
-			{ "<leader>f", group = "file" },
-			{ "<leader>fa", "<cmd>Telescope adjacent<CR>", desc = "Adjacent Files" },
-			{ "<leader>fe", "<cmd>Explore<CR>", desc = "Explore" },
-			{ "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>", desc = "Find File" },
-			{ "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Open Recent File" },
+      { '<leader>f', group = 'file' },
+      { '<leader>fa', '<cmd>Telescope adjacent<CR>', desc = 'Adjacent Files' },
+      { '<leader>fe', '<cmd>Explore<CR>', desc = 'Explore' },
+      { '<leader>ff', '<cmd>Telescope find_files hidden=true<cr>', desc = 'Find File' },
+      { '<leader>fr', '<cmd>Telescope oldfiles<cr>', desc = 'Open Recent File' },
 
-			{ "<leader>g", group = "git" },
-			{ "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<CR>", desc = "Current Line Blame" },
-			{ "<leader>gg", "<cmd>LazyGit<CR>", desc = "LazyGit" },
+      { '<leader>g', group = 'git' },
+      { '<leader>gb', '<cmd>Gitsigns toggle_current_line_blame<CR>', desc = 'Current Line Blame' },
+      { '<leader>gg', '<cmd>LazyGit<CR>', desc = 'LazyGit' },
 
-			{ "<leader>s", group = "search" },
-			{
-				"<leader>sb",
-				require("telescope.builtin").current_buffer_fuzzy_find,
-				desc = "Fuzzily search current buffer",
-			},
-			{ "<leader>sd", require("telescope.builtin").diagnostics, desc = "Search Diagnostics" },
-			{ "<leader>sd", require("telescope.builtin").diagnostics, desc = "Search Diagnostics" },
-			{ "<leader>sg", require("telescope.builtin").live_grep, desc = "Search with Grep" },
-			{ "<leader>sh", require("telescope.builtin").help_tags, desc = "Search Help" },
-			{ "<leader>sj", require("telescope.builtin").jumplist, desc = "Search Jumplist" },
-			{ "<leader>sr", require("telescope.builtin").lsp_references, desc = "Search References" },
-			{ "<leader>ss", require("telescope.builtin").lsp_document_symbols, desc = "Document Symbols" },
-			{ "<leader>st", "<cmd>TodoTelescope<CR>", desc = "Search TODO" },
-			{ "<leader>su", "<cmd>Telescope undo<CR>", desc = "Search Undo" },
-			{ "<leader>sc", require("telescope.builtin").grep_string, desc = "Search current Word" },
+      { '<leader>s', group = 'search' },
+      {
+        '<leader>sb',
+        require('telescope.builtin').current_buffer_fuzzy_find,
+        desc = 'Fuzzily search current buffer',
+      },
+      { '<leader>sd', require('telescope.builtin').diagnostics, desc = 'Search Diagnostics' },
+      { '<leader>sd', require('telescope.builtin').diagnostics, desc = 'Search Diagnostics' },
+      { '<leader>sg', require('telescope.builtin').live_grep, desc = 'Search with Grep' },
+      { '<leader>sh', require('telescope.builtin').help_tags, desc = 'Search Help' },
+      { '<leader>sj', require('telescope.builtin').jumplist, desc = 'Search Jumplist' },
+      { '<leader>sr', require('telescope.builtin').lsp_references, desc = 'Search References' },
+      { '<leader>ss', require('telescope.builtin').lsp_document_symbols, desc = 'Document Symbols' },
+      { '<leader>st', '<cmd>TodoTelescope<CR>', desc = 'Search TODO' },
+      { '<leader>su', '<cmd>Telescope undo<CR>', desc = 'Search Undo' },
+      { '<leader>sc', require('telescope.builtin').grep_string, desc = 'Search current Word' },
 
-			{ "<leader>t", group = "toggle" },
-			{ "<leader>tb", require("barbecue.ui").toggle, desc = "Barbecue (show code context winbar)" },
-			{ "<leader>tn", "<cmd>Neotree toggle<CR>", desc = "NeoTree" },
-			{ "<leader>ts", "<cmd>AerialToggle!<CR>", desc = "Symbols Tree" },
-			{ "<leader>tt", "<cmd>ToggleTerm size=30<CR>", desc = "Terminal" },
-		})
-	end,
+      { '<leader>t', group = 'toggle' },
+      { '<leader>tb', require('barbecue.ui').toggle, desc = 'Barbecue (show code context winbar)' },
+      { '<leader>tn', '<cmd>Neotree toggle<CR>', desc = 'NeoTree' },
+      { '<leader>ts', '<cmd>AerialToggle!<CR>', desc = 'Symbols Tree' },
+      { '<leader>tt', '<cmd>ToggleTerm size=30<CR>', desc = 'Terminal' },
+    }
+    -- Normal and Visual mode
+    wk.add {
+      mode = { 'n', 'x' },
+      { '<leader>c', group = 'code' },
+      {
+        '<leader>cx',
+        function()
+          require('telescope').extensions.refactoring.refactors()
+        end,
+        desc = 'Show Refactors',
+      },
+    }
+  end,
 }
