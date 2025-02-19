3d1b3b0dd9bf18258cd4b1871fd4996bd38c803a
Jack Allison <jacallis@cisco.com>
2025-02-19 10:58:23 -0500

    replace custom window sizes with edgy

---
+++ config/dot-config/nvim/lua/plugins/edgy.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/edgy.nvim",
+  event = "VeryLazy",
+  opts = {
+    options = {
+      left = { size = 50 },
+      bottom = { size = 20 },
+      right = { size = 50 },
+      top = { size = 10 },
+    },
+  },
+}
