5abaf9d4fff82154f7237200a535f347817592c4
Jack Allison <jacallis@cisco.com>
2025-04-07 16:01:37 -0400

    fix neotree behavior

---
+++ config/dot-config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,8 @@
+-- prevent neo-tree from opening files in edgy windows
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  optional = true,
+  opts = {
+    open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "edgy" },
+  },
+}
