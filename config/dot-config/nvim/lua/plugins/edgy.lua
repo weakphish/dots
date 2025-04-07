53b590d026a45696a56467b118960e0871121a02
Jack Allison <jacallis@cisco.com>
2025-04-07 13:23:58 -0400

    update / simplify nvim config

--- config/dot-config/nvim/lua/plugins/edgy.lua
+++ config/dot-config/nvim/lua/plugins/edgy.lua
@@ -1,7 +1,13 @@
--- Disable animations
+-- Disable animations and adjust sizes
 return {
   "folke/edgy.nvim",
   opts = {
+    options = {
+      left = { size = 50 },
+      bottom = { size = 25 },
+      right = { size = 50 },
+      top = { size = 10 },
+    },
     animate = {
       enabled = false,
     },
