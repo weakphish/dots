c9634332eccb68c40ebb28c1870853a57563bd21
Jack Allison <jacallis@cisco.com>
2025-04-10 10:06:54 -0400

    add mypy qf integration

--- config/dot-config/nvim/lua/config/keymaps.lua
+++ config/dot-config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>bo", "<cmd>BufferLinePick<CR>", { desc = "Open Buffer", remap = true })
+
+-- MyPy/Quickfix integration
+local function run_mypy_with_errorformat()
+  local current_file = vim.api.nvim_buf_get_name(0)
+  vim.cmd("compiler mypy") -- Ensure the 'mypy' compiler is set (see below)
+  vim.cmd("make " .. vim.fn.shellescape(current_file))
+  vim.cmd("copen")
+end
+
+vim.api.nvim_create_user_command("RunMyPyEF", run_mypy_with_errorformat, { desc = "Run MyPy with errorformat" })
+vim.keymap.set("n", "<leader>cp", ":RunMyPyEF<CR>", { desc = "Run MyPy with errorformat" })
