053a116bc07b6743d4f3962ce32111fa66ccd5c2
Jack Allison <jacallis@cisco.com>
2025-05-20 10:27:00 -0400

    obsidian config

--- config/dot-config/nvim/lua/plugins/obsidian.lua
+++ config/dot-config/nvim/lua/plugins/obsidian.lua
@@ -28,5 +28,23 @@ return {
         path = "~/vaults/work",
       },
     },
+    -- Optional, for templates (see below).
+    templates = {
+      folder = "templates/",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+    },
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "daily",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-%m-%d",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { "daily-notes" },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = "daily.md",
+    },
   },
 }
