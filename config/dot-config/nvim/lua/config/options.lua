5bc23690dd88b06c36d1600e4b74696f9d402735
Jack Allison <jacallis@cisco.com>
2025-06-05 12:47:46 -0400

    tabstop

--- config/dot-config/nvim/lua/config/options.lua
+++ config/dot-config/nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.shiftwidth = 4 -- Size of an indent
+vim.opt.tabstop = 4 -- size of a tab
 vim.wo.relativenumber = true
 
 -- LSP Server to use for Python.
