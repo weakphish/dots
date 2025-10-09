c8f09fdcfb535fa3e1050f5d8c622d8dc559dd20
Jack Allison <jacallis@cisco.com>
2025-10-09 16:22:25 -0400

    Simplify neovim conf

--- config/dot-config/nvim/lua/config/options.lua
+++ config/dot-config/nvim/lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
 vim.opt.shiftwidth = 4 -- Size of an indent
 vim.opt.tabstop = 4 -- size of a tab
 vim.wo.relativenumber = true
@@ -13,27 +14,3 @@ vim.g.lazyvim_python_ruff = "ruff"
 
 -- Disable lazyvim's default of syncing clipboard
 vim.opt.clipboard = ""
-
--- Set up Godot integration
--- Source: https://simondalvai.org/blog/godot-neovim/
--- paths to check for project.godot file
-local paths_to_check = { "/", "/../" }
-local is_godot_project = false
-local godot_project_path = ""
-local cwd = vim.fn.getcwd()
-
--- iterate over paths and check
-for key, value in pairs(paths_to_check) do
-  if vim.uv.fs_stat(cwd .. value .. "project.godot") then
-    is_godot_project = true
-    godot_project_path = cwd .. value
-    break
-  end
-end
-
--- check if server is already running in godot project path
-local is_server_running = vim.uv.fs_stat(godot_project_path .. "/server.pipe")
--- start server, if not already running
-if is_godot_project and not is_server_running then
-  vim.fn.serverstart(godot_project_path .. "/server.pipe")
-end
