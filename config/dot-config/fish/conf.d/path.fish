4f1b5b42372a1b90bf095cc38d9f209420e072f6
Jack Allison <jacallis@cisco.com>
2026-01-14 13:35:49 -0500

    Split out stuff more in fish conf

---
+++ config/dot-config/fish/conf.d/path.fish
@@ -0,0 +1,7 @@
+# Pulumi
+fish_add_path $HOME/.pulumi/bin
+
+# Local binaries (amp, pipx, etc.)
+fish_add_path $HOME/.local/bin
+
+
--- config/dot-config/fish/config.fish
+++ config/dot-config/fish/config.fish
@@ -1,5 +1,4 @@
 # Fish shell configuration
-# Noisy config: environment variables, PATH modifications, and tool initializations
 #
 # Modular config structure:
 #   conf.d/* 			- Config modules
@@ -14,33 +13,3 @@ fish_vi_key_bindings
 
 # Bat theme
 set -gx BAT_THEME base16
-
-#################
-### PATH Setup ###
-#################
-
-# Pulumi
-fish_add_path $HOME/.pulumi/bin
-
-# Local binaries (amp, pipx, etc.)
-fish_add_path $HOME/.local/bin
-
-##########################
-### Tool Initializations ###
-##########################
-
-# Zoxide (smart cd)
-zoxide init fish | source
-
-# Starship prompt
-starship init fish | source
-
-# uv (Python package manager)
-uv generate-shell-completion fish | source
-
-# fzf keybindings
-fzf --fish | source
-
-# VS Code shell integration
-string match -q "$TERM_PROGRAM" vscode
-and . (code --locate-shell-integration-path fish)
