714a6e45d79bd70c374caf100f9f3715a5d8ec8d
Jack Allison <jacallis@cisco.com>
2026-01-14 14:13:47 -0500

    remove config.fish env vars, use -U

--- config/dot-config/fish/config.fish
+++ config/dot-config/fish/config.fish
@@ -5,11 +5,5 @@
 #   conf.d/local.fish   - Machine-specific config (gitignored)
 #   functions/          - Autoloaded functions
 
-# Editor
-set -gx EDITOR nvim
-
 # Vi keybindings
 fish_vi_key_bindings
-
-# Bat theme
-set -gx BAT_THEME base16
